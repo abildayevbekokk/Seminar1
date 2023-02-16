427c3a9f62bbc27e32aa13ec44ff587671256245
Bekzhan <abildayevbekokk@gmail.com>
2023-02-16 19:25:54 +0600

    First lesson task

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+Console.WriteLine("Квадрат числа: {0}", Math.Pow(b, 2));
+
+
